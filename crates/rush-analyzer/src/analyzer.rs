5d92cdb6c3f6621037bc93a84f6fada6a34e79c6
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-11-04 15:02:32 +0100

    Implement some statements in analyzer

--- crates/rush-analyzer/src/analyzer.rs
+++ crates/rush-analyzer/src/analyzer.rs
@@ -1,8 +1,16 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use rush_parser::{Span, ast::{Type, ParsedProgram}};
+use rush_parser::{
+    ast::{ParsedFunctionDefinition, ParsedProgram, ParsedType, TypeKind},
+    Span,
+};
 
-use crate::{Diagnostic, DiagnosticLevel, ErrorKind};
+use crate::{
+    ast::{
+        AnnotatedFunctionDefinition, AnnotatedIdent, AnnotatedProgram, AnnotatedType, Annotation,
+    },
+    Diagnostic, DiagnosticLevel, ErrorKind,
+};
 
 pub struct Analyzer<'src> {
     pub functions: HashMap<&'src str, Function<'src>>,
@@ -12,8 +20,8 @@ pub struct Analyzer<'src> {
 
 pub struct Function<'src> {
     pub span: Span,
-    pub params: Vec<(&'src str, Type)>,
-    pub return_type: Type,
+    pub params: Vec<(AnnotatedIdent<'src>, AnnotatedType<'src>)>,
+    pub return_type: ParsedType<'src>,
 }
 
 #[derive(Debug)]
@@ -24,7 +32,7 @@ pub struct Scope<'src> {
 
 #[derive(Debug)]
 pub struct Variable {
-    pub type_: Type,
+    pub type_: TypeKind,
     pub span: Span,
 }
 
@@ -55,5 +63,61 @@ impl<'src> Analyzer<'src> {
             .push(Diagnostic::new(DiagnosticLevel::Error(kind), message, span))
     }
 
-    pub fn analyze(program: &ParsedProgram<'src>) ->
+    pub fn analyze(mut self, program: ParsedProgram<'src>) -> AnnotatedProgram<'src> {
+        let mut functions = vec![];
+        for function in program.functions {
+            functions.push(self.visit_function_declaration(function));
+        }
+        AnnotatedProgram {
+            span: program.span,
+            functions,
+        }
+    }
+
+    fn visit_function_declaration(
+        &mut self,
+        function: ParsedFunctionDefinition<'src>,
+    ) -> AnnotatedFunctionDefinition<'src> {
+        if self.functions.get(function.name.value).is_some() {
+            self.error(
+                ErrorKind::Semantic,
+                "duplicate function definition".to_string(),
+                function.name.span,
+            )
+        }
+
+        let mut params = vec![];
+        let mut param_names = HashSet::new();
+        for param in &function.params {
+            // check for duplicate function parameters
+            if !param_names.insert(param.0.value) {
+                self.error(
+                    ErrorKind::Semantic,
+                    "duplicate parameter name".to_string(),
+                    param.0.span,
+                )
+            }
+            params.push((
+                AnnotatedIdent {
+                    span: param.0.span,
+                    annotation: Annotation::new(param.1.value, false),
+                    value: param.0.value,
+                },
+                AnnotatedType {
+                    span: param.1.span,
+                    annotation: Annotation::new(param.1.value, false),
+                    value: param.1.value,
+                },
+            ))
+        }
+        self.functions.insert(
+            function.name.value,
+            Function {
+                span: function.span,
+                params,
+                return_type: function.return_type,
+            },
+        );
+        todo!()
+    }
 }
