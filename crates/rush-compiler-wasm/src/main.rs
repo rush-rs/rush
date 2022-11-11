268ac080086a07a02dfa1f7943bca302e50aa426
RubixDev <silas.groh@t-online.de>
2022-11-11 17:14:46 +0100

    wasm: Add simple `main.rs` for testing

---
+++ crates/rush-compiler-wasm/src/main.rs
@@ -0,0 +1,10 @@
+use std::{env, fs, time::Instant};
+
+fn main() {
+    let start = Instant::now();
+    let path = env::args().nth(1).unwrap();
+    let code = fs::read_to_string(path).unwrap();
+    let bytes = rush_compiler_wasm::compile(&code).unwrap().0;
+    fs::write("output.wasm", bytes).unwrap();
+    println!("{:?}", start.elapsed());
+}
