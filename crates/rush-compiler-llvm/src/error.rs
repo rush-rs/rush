feb19f395edd4ecdaab7d87827d34c46b563050d
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-11-11 10:54:35 +0100

    llvm: Add `Display` implementation on `Error`

--- crates/rush-compiler-llvm/src/error.rs
+++ crates/rush-compiler-llvm/src/error.rs
@@ -1,4 +1,6 @@
-use inkwell::{support::LLVMString, targets::TargetTriple};
+use std::fmt::Display;
+
+use inkwell::support::LLVMString;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -6,6 +8,19 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
     Llvm(String),
     NoTarget,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Llvm(message) => format!("llvm: {message}"),
+                Self::NoTarget => "invalid target: no such target".to_string(),
+            }
+        )
+    }
 }
 
 impl From<LLVMString> for Error {
