8890d9ede01f966c8764810cfdab417050af3728
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2023-01-19 19:54:33 +0100

    cli: When building using LLVM, also produce binary file

--- crates/rush-cli/src/x86.rs
+++ crates/rush-cli/src/x86.rs
@@ -11,42 +11,29 @@ use tempfile::tempdir;
 
 use crate::cli::{BuildArgs, RunArgs};
 
-pub fn compile(ast: AnalyzedProgram, args: BuildArgs) -> anyhow::Result<()> {
+pub fn compile(ast: AnalyzedProgram, args: BuildArgs) -> anyhow::Result<PathBuf> {
     let asm = Compiler::new().compile(ast);
 
     // get output path
-    let output = match args.output_file {
+    let output_path = match args.output_file {
         Some(out) => out,
-        None => {
-            let mut path = PathBuf::from(
-                args.path
-                    .file_stem()
-                    .with_context(|| "cannot get filestem of input file")?,
-            );
-            path.set_extension("s");
-            path
-        }
+        None => PathBuf::from(
+            args.path
+                .file_stem()
+                .with_context(|| "cannot get filestem of input file")?,
+        ),
     };
 
-    fs::write(&output, asm)
-        .with_context(|| format!("cannot write to `{file}`", file = output.to_string_lossy()))?;
-
-    Ok(())
-}
-
-pub fn run(ast: AnalyzedProgram, args: RunArgs) -> anyhow::Result<i64> {
-    #[cfg(not(target_arch = "x86_64"))]
-    bail!("not running on `x86_64`");
+    let mut asm_path = output_path.clone();
+    asm_path.set_extension("s");
+    fs::write(&asm_path, asm).with_context(|| {
+        format!(
+            "cannot write to `{file}`",
+            file = output_path.to_string_lossy()
+        )
+    })?;
 
     let tmpdir = tempdir()?;
-
-    let mut args: BuildArgs = args.try_into()?;
-
-    let asm_path = tmpdir.path().join("output.s");
-    args.output_file = Some(asm_path.clone());
-    compile(ast, args)?;
-
-    let bin_path = tmpdir.path().join("output");
     let libcore_file_path = tmpdir.path().join("libcore.a");
     let libcore_bytes = include_bytes!("../../rush-compiler-x86-64/corelib/libcore.a");
     fs::write(&libcore_file_path, libcore_bytes)?;
@@ -56,7 +43,7 @@ pub fn run(ast: AnalyzedProgram, args: RunArgs) -> anyhow::Result<i64> {
         .arg(libcore_file_path)
         .arg("-nostdlib")
         .arg("-o")
-        .arg(&bin_path)
+        .arg(&output_path)
         .stderr(Stdio::piped())
         .spawn()?;
 
@@ -70,7 +57,26 @@ pub fn run(ast: AnalyzedProgram, args: RunArgs) -> anyhow::Result<i64> {
         ),
     }
 
-    match Command::new(bin_path).output()?.status.code() {
+    Ok(output_path)
+}
+
+pub fn run(ast: AnalyzedProgram, args: RunArgs) -> anyhow::Result<i64> {
+    #[cfg(not(target_arch = "x86_64"))]
+    bail!("not running on `x86_64`");
+
+    let args: BuildArgs = args.try_into()?;
+    let bin_output = compile(ast, args)?;
+
+    match Command::new(
+        bin_output
+            .canonicalize()
+            .with_context(|| "could not canonicalize output path")?,
+    )
+    .output()
+    .with_context(|| "could not run binary")?
+    .status
+    .code()
+    {
         Some(code) => Ok(code as i64),
         None => Err(anyhow!("could not get exit-code of rush bin process")),
     }
